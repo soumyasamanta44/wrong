2d6191aba9b789619cf8e916005a150ee044c443
soumyasamanta44 <soumyasamanta2018@gmail.com>
2018-07-04 13:54:27 +0000

    message

---
+++ pyhton.py
@@ -0,0 +1,6 @@
+num1 = input("eneter the first number:")
+num2 = input("enter the second number:")
+if num1 > num2:
+    print " the num1 is big + 100" ,num1+100
+else:
+     print " the num2 is big + 100",num2+100
